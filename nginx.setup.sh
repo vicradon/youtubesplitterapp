2ca9d8ebeec72c014b6b53c6ef167be827d6a088
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-06-27 05:39:44 +0100

    chore: add nginx config

---
+++ nginx.setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+sudo apt update \
+&& sudo apt install nginx -y \
+&& sudo snap install --classic certbot
+
+servername=youtubedl.osinachi.me
+port=5000
+
+sudo touch /etc/nginx/conf.d/$servername.conf
+sudo tee /etc/nginx/conf.d/$servername.conf > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $servername www.$servername;
+    
+    location / {
+        proxy_pass http://localhost:$port;
+    }
+}
+EOF
+
+sudo nginx -t
+sudo nginx -s reload
+
+sudo certbot --nginx
